9ce0f1936290d3a0994c8e9a49edbdd6f717048a
fcj <fcjljy@gmail.com>
2015-04-15 14:31:48 +0800

    add lesson3 for run on local

--- learn_from_alsotang/lesson3/app.js
+++ learn_from_alsotang/lesson3/app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var superagent = require("superagent");
 var cheerio = require("cheerio");
-
+var fs = require("fs")
 
 var app = express();
 
 
-app.get("/",function(req,res,next){
+/*app.get("/",function(req,res,next){
 	// using cnodejs get an error about "connect ECONNREFUSED", so I change the origin program
 	superagent.get("https://www.google.com/")  // https://cnodejs.org/
 	.end(function(err,sres){
@@ -27,10 +27,41 @@ app.get("/",function(req,res,next){
 
 			res.send(items);
 		});
+});*/
+
+app.get('/', function (req, res, next) {
+  superagent.get('http://localhost:6776/cnode.html')
+    .end(function (err, sres) {
+      if (err) {
+        return next(err);
+      }
+     // console.log(sres.text);
+      var $ = cheerio.load(sres.text);
+      var items = [];
+      $('#topic_list .topic_title').each(function (idx, element) {
+        var $element = $(element);
+        items.push({
+          title: $element.attr('title'),
+          href: $element.attr('href')
+        });
+      });
+
+      res.send(items);
+    });
 });
 
 
+app.get('/cnode.html',function(req,res,next){
+	//var text = 
+	//res.send(text);
+	fs.readFile("cnode.html",function(err,data){
+		//console.log(data);
+		res.send(data.toString('utf-8'));  // or res.send("" + data);
+	});
+
+
+});
 
 app.listen(6776,function(req,res){
-	console.log("Listening port 6776");
+	console.log("Listening port 6776 in Lesson3");
 });
